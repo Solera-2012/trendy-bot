4129bab5ff2eab2a855fd925a20451c5d749b6a3
Nathaniel Saul <nat@saulgill.com>
2016-02-08 22:22:49 -0800

    Adding my secret_key lockbox - temp solution

--- scraper/sandbox/twitter-api-test.py
+++ scraper/sandbox/twitter-api-test.py
@@ -1,14 +1,17 @@
 from secret_keys import secret_keys
 import tweepy
 
-key = secret_keys.secret_keys()
 
+#load keys from secret file
+key = secret_keys.secret_keys()
+print(key)
+
+#authenticate
 auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
 auth.set_access_token(key.access_token_key, key.access_token_secret)
-
 api = tweepy.API(auth)
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-	print(tweet.text)
 
+#grab tweets from user
+new_tweets = api.user_timeline(screen_name="ropthe_", count=2)
 
+print(new_tweets)
