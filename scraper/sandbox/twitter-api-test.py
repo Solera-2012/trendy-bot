c941454aa708611786384349453f0a3fe50a3b42
Nathaniel Saul <nat@saulgill.com>
2016-02-08 19:37:58 -0800

    First example of pulling tweets

--- scraper/sandbox/twitter-api-test.py
+++ scraper/sandbox/twitter-api-test.py
@@ -1,8 +1,14 @@
 from secret_keys import secret_keys
-
+import tweepy
 
 key = secret_keys.secret_keys()
-print(key.consumer_key)
-print(key.consumer_secret)
-print(key.access_token_key)
-print(key.access_token_secret)
+
+auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
+auth.set_access_token(key.access_token_key, key.access_token_secret)
+
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+	print(tweet.text)
+
+
