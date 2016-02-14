abb68ff2ffaaa94be267e036cc6ff0b0764ede6a
Nathaniel Saul <nat@saulgill.com>
2016-02-14 00:14:12 -0800

    wrapped spell checking in a class and built test suite

---
+++ scraper/sandbox/spell_check/test_SpellChecker.py
@@ -0,0 +1,35 @@
+import SpellChecker 
+import unittest
+
+class TestSpellChecker(unittest.TestCase):
+	def setUp(self):
+		self.sp = SpellChecker.SpellChecker()
+
+	def test_wordsAreCorrected(self):
+		word = self.sp.correct("feling")
+		self.assertEqual(word, "feeling")
+
+	def test_words(self): 
+		text = "hi this is a word that I like to do"
+		words = self.sp.words(text)
+		words_list = ['hi', 'this', 'is', 'a', 'word', 'that', 'i', 'like', 'to', 'do']
+		self.assertEqual(words_list, words)		
+
+	def test_train(self):
+		pass
+
+	def edits1(self):
+		pass
+
+	def known_edits2(self):
+		pass
+	
+	def known(self): 
+		pass
+
+	def correct(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
